2a41d2f618ce21fa66380215c5722a833bbfb462
MikołajNarloch <mikinarloch@gmail.com>
2023-11-06 23:19:33 +0100

    Remove test article 4

--- src/test/java/org/qmwebsite/pages/blog/de/Article40.java
+++ src/test/java/org/qmwebsite/pages/blog/de/Article40.java
@@ -30,7 +30,7 @@ public class Article40 extends BasePage {
     }
     public Article40 checkSustainableLink() {
         checkLink(sustainableXpath,sustainablePageTitle);
-
+//tesr
         return this;
     }
 }
